ad1e22ebbc64a87e56c6291307ab9171a0b4e236
petereberlecom <git@peter-eberle.com>
2016-05-10 09:12:38 +0200

    fixed merges

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,9 @@ cd dist
 git init
 
 # inside this git repo we'll pretend to be a new user
-<<<<<<< HEAD
-git config user.name "petereberlecom"
-git config user.email "git@peter-eberle.com"
-=======
+
 git config user.name "stefanbuck"
 git config user.email "git@stefanbuck.com"
->>>>>>> be9bacd45f75971a57074b756fd955d2a0871f60
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
@@ -28,16 +24,9 @@ git commit -m "Deploy to GitHub Pages"
 
 # exit 0;
 
-<<<<<<< HEAD
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any distput to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-=======
+
 # Force push from the current repo's source branch to the remote
 # repo's master branch. (All previous history on the master branch
 # will be lost, since we are overwriting it.) We redirect any distput to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
->>>>>>> be9bacd45f75971a57074b756fd955d2a0871f60
--- readme.md
+++ readme.md
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 # octo-linker/www [![Build Status](https://travis-ci.org/OctoLinker/OctoLinker.github.io.svg?branch=master)](https://travis-ci.org/OctoLinker/OctoLinker.github.io)
-=======
-# OctoLinker website [![Build Status](https://travis-ci.org/OctoLinker/OctoLinker.github.io.svg?branch=master)](https://travis-ci.org/OctoLinker/OctoLinker.github.io)
->>>>>>> be9bacd45f75971a57074b756fd955d2a0871f60
 
 website for octo-linker
 
 
-<<<<<<< HEAD
+
 ## usage
-=======
-## Usage
->>>>>>> be9bacd45f75971a57074b756fd955d2a0871f60
+
 
 ### install
 
